169b56894ad7021f474360ed6d17e4030873fd54
GraniteBeltWeb <40533161+GraniteBeltWebAndSoftwareSolutions@users.noreply.github.com>
2019-05-29 22:08:43 +1000

    Add files via upload
    remember to 
    npm install express mysql cors --save
    or just npm install as package.json tells it what to do.
    also 
    npm install nodemon -g --save

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require ('express');
+const cors = require('cors');
+const mysql = require('mysql');
+ 
+const app = express();
+
+const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM library';
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'matthewstlibrary'
+});
+
+connection.connect(err =>{
+	if(err){
+		return err;
+	}
+})
+
+
+app.use(cors());
+
+app.get('/', (req, res)=> {
+	res.send('go to /products')
+	});
+
+
+app.get('/products', (req, res)=> {
+		connection.query(SELECT_ALL_PRODUCTS_QUERY, (err, results) => {
+		if(err){
+			return res.send(err)
+		}
+		else{
+			return res.json({
+			data: results	
+			})
+		}
+	});
+});
+
+app.get('/smoothly', (req, res)=> {
+	res.send('Smooth! You are on the Smooth page')
+	});
+
+
+
+app.listen(4000, ()=> {
+	console.log('Product server listening on port 4000')
+	
+	
+})
